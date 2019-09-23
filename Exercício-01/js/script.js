e745644a91da1a618e92ee9b286aee62cfcad37e
RaissaMariaB <raissamaria.sb@gmail.com>
2019-09-23 17:07:09 -0300

    adiciona JS nos exercícios 1 e 2

---
+++ Exercício-01/js/script.js
@@ -0,0 +1,45 @@
+// ciar elementos dessa lista pelo js para o html
+
+const alunas = ["Simara", "lia", "aline", "bah", "thais", "dry", "manu"]
+
+// capturar ul do html - a div mãe - para isso se cria uma variável para guardar a div mãe, e usa-se o getElementBy
+
+let divMae = document.getElementById("lista-alunas")
+
+// * no meu petodo eu tenho que especificar qual, nesse caso era id; e o document é o acesso ao DOM - documet. *
+
+
+//percorrer array
+
+for (i=0; i<alunas.length; i++){ // dentro do proprio for você pode criar a propria 
+    //cria os itens da lista pq eu vou atribuir eles ao html
+    let itemLista = document.createElement("li")
+    //colaca conteudo dentro da lista
+    itemLista.textContent = alunas[i]
+    //eu to criando conteudo para lista e atribuindo a conteudo , de cada item ja existente da variável array
+    divMae.appendChild(itemLista)
+    //chamamos a let que esta guardando a div mae e botamos uma funcao a ela - metodo - e linka com o filho
+    
+}
+
+// contando cliques no título
+
+//mirar a tag e capturar o valor - o querySelector pega o primeiro item dentro daquela classe chamada
+let meuTitulo = document.querySelector(".titulo")
+//escutar o evento no elemento
+let soma = 0
+
+meuTitulo.addEventListener("click", function logar(){ 
+    console.log(soma = soma++)
+})
+// quando existe uma função dentro de outra função é chama de callback, seja ela criada dentro do parâmetro ou fora
+// e colocada pra dentro dela
+//no evento que irá ocorrer eu preciso colocar dentro do parâmetro oq eu quero que ocorra, exite documentação de 
+// opções
+
+
+
+
+
+
+
